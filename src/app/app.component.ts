ce4f5b303c9fb935f5b3f4f179655bcf9efba9c6
mohamedMQ <maqbour.moh@gmail.com>
2025-03-05 17:16:09 +0000

    chore: structure the folders and files for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+// import { DashboardModule } from './dashboard/dashboard.module'
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    // RouterOutlet,
+    // DashboardModule,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
